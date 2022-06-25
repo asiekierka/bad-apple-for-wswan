c39dca7100208d22cb309e51149b54236d5eec58
Adrian Siekierka <kontakt@asie.pl>
2022-06-25 13:30:07 +0200

    update toolchain version, license WS code as MIT

--- src/main.c
+++ src/main.c
@@ -1,3 +1,24 @@
+/* Copyright (c) 2022 Adrian "asie" Siekierka
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
@@ -111,7 +132,7 @@ int main(void) {
 	// configure display
 	outportw(IO_DISPLAY_CTRL, 0);
 
-	video_set_gray_lut(GRAY_LUT(0, 2, 5, 6, 8, 10, 13, 15));
+	video_shade_lut_set(GRAY_LUT(0, 2, 5, 6, 8, 10, 13, 15));
 	outportw(IO_SCR_PAL_0, 0x7520);
 	outportw(IO_SCR_PAL_1, 0x0257);
 
@@ -194,7 +215,7 @@ int main(void) {
 
 	// copy tiles and map
 	memcpy(MEM_TILE(0), asset_map(ASSET_END_CARD_TILES_BIN), ASSET_END_CARD_TILES_BIN_SIZE);
-	video_put_screen_map(curr_screen, asset_map(ASSET_END_CARD_MAP_BIN), 0, 0, 28, 18);
+	video_screen_put_map(curr_screen, asset_map(ASSET_END_CARD_MAP_BIN), 0, 0, 28, 18);
 
 	for (int i = 0; i < fade_from_black_len; i++) {
 		vblank_ticks = 0;
